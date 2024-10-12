5d1c4e4c5d5937408b285272bc94e5fdf3d1b418
Gabriel <saulymarclash12@gmail.com>
2024-10-12 13:32:21 -0400

    "Ejercicio Menu terminado"

--- JAVASCRIPT/JAVASCRIPT/JAVASCRIPT/bucles/menu.js
+++ JAVASCRIPT/JAVASCRIPT/JAVASCRIPT/bucles/menu.js
@@ -10,10 +10,61 @@ tenga en cuenta que el menu se va a repetir hasta que el usuario
 indique la opcion de salir
 */
 
-let op, numero1, numero2
+var op;
+var rep = true;
+var num1, num2
+var resultado
+var final
 
-op = prompt('Calculadora: \n 1. Suma \n 2. Resta \n 3.Multiplicar \n 4. Dividir \n 5. Salir')
+do{
+    op = parseInt(prompt('Calculadora: \n 1. Suma \n 2. Resta \n 3.Multiplicar \n 4. Dividir \n 5. Salir'))
+    //console.log(op);
 
-switch (op) {
-    
-}
+
+    switch(op){
+        case 1:
+            //suma
+            //console.log("opcion suma")
+            num1 = parseInt(prompt("Ingrese el valor 1"))
+            num2 = parseInt(prompt("Ingrese el valor 2"))
+            resultado = num1 + num2
+            alert("El resultado de la suma es: " + resultado)
+            break;
+        case 2:
+            //resta
+            //console.log("opcion resta")
+            num1 = parseInt(prompt("Ingrese el valor 1"))
+    num2 = parseInt(prompt("Ingrese el valor 2"))
+            resultado = num1 - num2
+            alert("El resultado de la resta es: " + resultado)
+            break;
+        case 3:
+            //multiplicacion
+            //console.log("opcion multiplicacion")
+            num1 = parseInt(prompt("Ingrese el valor 1"))
+    num2 = parseInt(prompt("Ingrese el valor 2"))
+            resultado = num1 * num2
+            alert("El resultado de la multiplicacion es: " + resultado)
+            break;
+        case 4:
+            //dividir
+            //console.log("opcion dividir")
+            num1 = parseInt(prompt("Ingrese el valor 1"))
+    num2 = parseInt(prompt("Ingrese el valor 2"))
+            if( num2 === 0){
+                alert("No se puede dividir entre cero (0)")
+            } else{
+                resultado = num1 / num2
+                alert("El resultado de la division es: " + resultado)
+            }
+            break;
+        case 5:
+            //salir
+            //console.log("opcion salir")
+            rep=false;
+            break;
+        default:
+            prompt("Opción inválida. Por favor, elige entre 1 y 5.")
+            break;
+    }
+} while (rep)
