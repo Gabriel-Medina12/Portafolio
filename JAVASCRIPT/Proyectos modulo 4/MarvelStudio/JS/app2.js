803acb4eb199a0987be564d1e8cba476744183bd
Gabriel <saulymarclash12@gmail.com>
2024-11-17 23:17:19 -0400

    Logramos hacer que se mostrara cada informacion con su respectivo boton, ahora solo falta ocultar el anterior y colocarle el css

--- JAVASCRIPT/Proyectos modulo 4/MarvelStudio/JS/app2.js
+++ JAVASCRIPT/Proyectos modulo 4/MarvelStudio/JS/app2.js
@@ -10,6 +10,8 @@ const getData = async (url, offset = 0) => {
     const data = response.json()
     return data;
 }
+
+
 function addData(data){
     const dataContainer = document.getElementById('personajes');
     dataContainer.innerHTML = "";
@@ -25,9 +27,142 @@ function addData(data){
         `
     })
 }
+const personajes = document.getElementById('character');
+personajes.addEventListener('click', ()=>{
+    getData('https://gateway.marvel.com/v1/public/characters', offset)
+    .then(data =>{
+        console.log(data)
+        addData(data.data.results)
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
 
 
+function addData(data2){
+    const dataContainer = document.getElementById('comic');
+    dataContainer.innerHTML = "";
 
+    data2.forEach(item =>{
+        dataContainer.innerHTML +=`
+        <div class="card-item">
+            <img src="${item.thumbnail.path + "." + item.thumbnail.extension}">
+            <h2>${item.title ? item.title : "No se encontró titulo"}</h2>
+            <p>${item.issueNumber ? item.issueNumber : "No se encontro numero de comic"}</p>
+            <p>${item.variantDescription ? item.variantDescription : "No hay descripcion"}</p>
+        </div>
+        `
+    })
+}
+const comics = document.getElementById('comics');
+comics.addEventListener('click', ()=>{
+    getData('https://gateway.marvel.com/v1/public/comics', offset)
+    .then(data2 =>{
+        console.log(data2)
+        addData(data2.data.results)
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+
+function addData(data3){
+    const dataContainer = document.getElementById('creador');
+    dataContainer.innerHTML = "";
+
+    data3.forEach(item =>{
+        dataContainer.innerHTML +=`
+        <div class="card-item">
+            <img src="${item.thumbnail.path + "." + item.thumbnail.extension}">
+            <h2>${item.firstName ? item.firstName : "No se encontró titulo"}</h2>
+        </div>
+        `
+    })
+}
+const creadores = document.getElementById('creadores');
+creadores.addEventListener('click', ()=>{
+    getData('https://gateway.marvel.com/v1/public/creators', offset)
+    .then(data3 =>{
+        console.log(data3)
+        addData(data3.data.results)
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+
+function addData(data4){
+    const dataContainer = document.getElementById('evento');
+    dataContainer.innerHTML = "";
+
+    data4.forEach(item =>{
+        dataContainer.innerHTML +=`
+        <div class="card-item">
+            <img src="${item.thumbnail.path + "." + item.thumbnail.extension}">
+            <h2>${item.title ? item.title : "No se encontró titulo"}</h2>
+            <p>${item.description ? item.description : "No hay descripcion"}</p>
+        </div>
+        `
+    })
+}
+const eventos = document.getElementById('eventos');
+eventos.addEventListener('click', ()=>{
+    getData('https://gateway.marvel.com/v1/public/events', offset)
+    .then(data4 =>{
+        console.log(data4)
+        addData(data4.data.results)
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+
+function addData(data5){
+    const dataContainer = document.getElementById('serie');
+    dataContainer.innerHTML = "";
+
+    data5.forEach(item =>{
+        dataContainer.innerHTML +=`
+        <div class="card-item">
+            <img src="${item.thumbnail.path + "." + item.thumbnail.extension}">
+            <h2>${item.title ? item.title : "No se encontró titulo"}</h2>
+            <p>${item.description ? item.description : "No hay descripcion"}</p>
+        </div>
+        `
+    })
+}
+const series = document.getElementById('series');
+series.addEventListener('click', ()=>{
+    
+    getData('https://gateway.marvel.com/v1/public/series', offset)
+    .then(data5 =>{
+        console.log(data5)
+        addData(data5.data.results)
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
+
+
+function addData(data6){
+    const dataContainer = document.getElementById('historia');
+    dataContainer.innerHTML = "";
+
+    data6.forEach(item =>{
+        dataContainer.innerHTML +=`
+        <div class="card-item">
+            <img src="${item.thumbnail.path + "." + item.thumbnail.extension}">
+            <h2>${item.title ? item.title : "No se encontró titulo"}</h2>
+            <p>${item.description ? item.description : "No hay descripcion"}</p>
+        </div>
+        `
+    })
+}
+const historias = document.getElementById('historias');
+historias.addEventListener('click', ()=>{
+    
+    getData('https://gateway.marvel.com/v1/public/stories', offset)
+    .then(data6 =>{
+        console.log(data6)
+        addData(data6.data.results)
+    })
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+})
 
 
 const back = document.getElementById('back');
@@ -35,22 +170,22 @@ const next = document.getElementById('next');
 back.addEventListener('click', ()=>{
     if(offset > 0){
         offset -=20;
-        window.scrollTo({ top: 0, behavior: 'smooth' });
     }
     getData('https://gateway.marvel.com/v1/public/characters', offset)
     .then(data => {
         //console.log(data.data.results)
         addData(data.data.results)
     });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 })
 next.addEventListener('click', ()=>{
     offset += 20;
-    window.scrollTo({ top: 0, behavior: 'smooth' });
     getData('https://gateway.marvel.com/v1/public/characters', offset)
     .then(data => {
         //console.log(data.data.results)
         addData(data.data.results)
     });
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 })
 getData('https://gateway.marvel.com/v1/public/characters', offset)
     .then(data => {
@@ -58,7 +193,11 @@ getData('https://gateway.marvel.com/v1/public/characters', offset)
         addData(data.data.results)
     });
 
+function mostrar(){
+    if(data, data2, data3, data4, data5, data6){
 
+    }
+}
 //tarea arrglar los estilos de la pagina de marvel
 //barra de navegacion: personajes, comics, creadores, eventos, series, historias
 //
