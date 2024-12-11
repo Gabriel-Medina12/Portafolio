d1a50eb486658a65094a60ec99ff1b5a381c7dcd
Gabriel Medina <saulymarclash12@gmail.com>
2024-12-11 15:14:17 -0400

    Creacion del bot de discord

---
+++ JAVASCRIPT/Modulo5/kny-boot/index.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () =>{
+    console.log('sesion iniciada');
+});
+
+client.on('message', (mensaje)=>{
+    if (mensaje.content == 'ping'){
+       mensaje.reply('...');
+    }
+}) 
+client.login()
