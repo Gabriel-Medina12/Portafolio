35955a65af8c4595b8bd9b95141133e56c4f479f
Gabriel Medina <saulymarclash12@gmail.com>
2025-02-05 16:32:44 -1200

    cambios variados

--- JAVASCRIPT/Modulo6/nuevo_server/controllers/users.js
+++ JAVASCRIPT/Modulo6/nuevo_server/controllers/users.js
@@ -27,4 +27,40 @@ router.get('/:id', (request, response)=>{
     })
 })
 
+router.post('/', (request, response)=>{
+    const user = request.body;
+    console.log(user)
+    usuarios.createUser(user, (error, result)=>{
+        if (error){
+            console.log('error en la consulta')
+            response.status(500).send('Error en la consulta')
+            return;
+        }
+        response.send({mensaje: 'Usuario Registrado'})
+    })
+})
+router.put('/', (request, response)=>{
+    const user = request.body;
+    console.log(user)
+    usuarios.updateUser(user, (error, result)=>{
+        if (error){
+            console.log('error en la consulta')
+            response.status(500).send('Error en la consulta')
+            return;
+        }
+        response.send({mensaje: 'Usuario actualizado'})
+    })
+})
+router.delete('/:id', (request, response)=>{
+    usuarios.deleteUser(request.params.id, (error, result)=>{
+        if(error){
+            console.log('error al ejecutar la consulta');
+            response.status(500).send('error al ejercutar la consulta')
+            return 
+        }
+        response.send({mensaje: 'Usuario eliminado exitosamente'})
+    })
+})
+
+
 module.exports = router
--- JAVASCRIPT/Modulo6/nuevo_server/models/users.js
+++ JAVASCRIPT/Modulo6/nuevo_server/models/users.js
@@ -13,14 +13,17 @@ const usuarios = {
         const consulta = `SELECT * FROM usuarios WHERE nombre = ${nombre}`;
         db.query(consulta = [nombre], callback)
     },
-    createUser: function(){
-
+    createUser: function(usuario, callback){
+        const consulta = `INSERT INTO usuarios (nombre, apellido, email, direccion) VALUES (${usuario.nombre}, ${usuario.apellido}, ${usuario.email}, ${usuario.direccion} )`
+        return db.query(consulta, callback)
+    },  
+    updateUser: function(usuario, callback){
+        const consulta = `UPDATE usuarios SET nombre = '${usuario.nombre}', apellido = '${usuario.apellido}', email = '${usuario.email}' WHERE id = ${usuario.id}`
+        return db.query(consulta, callback);
     },
-    deleteUser: function(){
-
-    },
-    updateUser: function(){
-
+    deleteUser: function(id, callback){
+        const consulta = `DELETE FROM usuarios WHERE id = ${id}`
+        return db.query(consulta, callback)
     }
 }
 
