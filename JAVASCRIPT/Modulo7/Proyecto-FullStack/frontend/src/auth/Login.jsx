08ce6890a684e573f7a8a05a8cb21a56052fc1d0
Gabriel Medina <saulymarclash12@gmail.com>
2025-02-21 08:09:46 -0400

    Ajustes en el proyecto FullStack que estamos haciendo en clases, el login y el register

---
+++ JAVASCRIPT/Modulo7/Proyecto-FullStack/frontend/src/auth/Login.jsx
@@ -0,0 +1,48 @@
+import Button from "../components/auth/Button"
+
+const Login = ()=>{
+
+    const loginFunction = async(e)=>{
+        e.preventDefault();
+
+
+        fetch('http://localhost:3000/auth/login',{
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: {}
+        })
+        .then(response => response.json())
+        .then(data =>{
+            alert(data.message)
+        })
+    }
+
+    return(
+        <>
+        <div className="container container-auth">
+            <div className="row w-100">
+                <div className="col-4 ms-auto me-auto card p-4">
+                    <h1>Login</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                    <form action="" onSubmit={loginFunction}>
+                        <div className="mb-3">
+                            <label htmlFor="email" className="form-label">Email</label>
+                            <input type="email" className="form-control" id="email" placeholder="Email"/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password" className="form-label">Password</label>
+                            <input type="password" className="form-control" id="password" placeholder="Password"/>
+                        </div>
+                        <Button text='Login' color='info'></Button>
+
+                    </form>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Login
---
+++ JAVASCRIPT/Modulo7/Proyecto-FullStack/frontend/src/components/auth/Button.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+const Button = ({text, color}) =>{
+
+    return (
+        <>
+            <button className={"btn btn-"+color} >
+                {text ? text : 'Button'}
+
+
+            </button>
+        </>
+    )
+
+}
+
+export default Button
