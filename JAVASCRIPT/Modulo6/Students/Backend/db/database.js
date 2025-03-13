38e846546d498890d098cc9daa28f2c3afc0ba51
Gabriel Medina <saulymarclash12@gmail.com>
2025-03-13 01:51:10 -0400

    Ultimos detalles en el proyecto, ya esta full terminado

--- JAVASCRIPT/Modulo6/Students/Backend/db/database.js
+++ JAVASCRIPT/Modulo6/Students/Backend/db/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'MandyORG/2005',
+    password: '',
     database: 'registor_students',
     waitForConnections: true,
     connectionLimit: 10,
